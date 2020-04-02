4e9918cd616cc6ace7e881955bbe9d7a1ec83465
Chow Sin <chowsin@wittydata.com>
2020-04-02 19:29:13 +0800

    remove proxy

--- lib/src/NotificationsAndroid.ts
+++ lib/src/NotificationsAndroid.ts
@@ -1,18 +1,8 @@
 import { Commands } from './commands/Commands';
-import { Platform } from 'react-native';
-import 'proxy-polyfill';
 
 export class NotificationsAndroid {
   constructor(private readonly commands: Commands) {
-    /*return new Proxy(this, {
-      get(target, name) {
-        if (Platform.OS === 'android') {
-          return (target as any)[name];
-        } else {
-          return () => {};
-        }
-      }
-    });*/
+    
   }
 
   /**
